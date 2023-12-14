226fea875a0a8132244a117b76ce63f4ab6ce72e
ismailFY0204 <153852706+ismailFY0204@users.noreply.github.com>
2023-12-15 02:22:51 +0800

    Add files via upload

---
+++ Metode_Luminosity.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+img_path = 'Ismail_.jpeg'
+img = cv2.imread(img_path)
+
+# Periksa apakah gambar berhasil dibaca
+if img is None:
+    print(f'Gagal membaca gambar dari path: {img_path}')
+else:
+    # Cetak bentuk gambar
+    print(img.shape)
+
+    fix_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(fix_img)
+    plt.show()
+
+    R, G, B = fix_img[:,:,0], fix_img[:,:,1], fix_img[:,:,2]
+    print(np.array(fix_img))
+
+
+    lumi_img = (0.2126*R) + (0.7152*G) + (0.0722*B)
+    print(lumi_img)
+
+    plt.axis('off')
+    plt.imshow(gray_img, cmap = 'gray')
+    plt.savefig('Metode Luminosity.jpg' , bbox_inches = 'tight')
